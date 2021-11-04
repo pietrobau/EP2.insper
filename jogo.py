d20efa6311f507f2cb7257e98cb1f2ee3f7da158
matheuslv <matheuslv@al.insper.edu.br>
2021-11-04 15:17:40 -0300

    Dando as peças para os jogadores

--- jogo.py
+++ jogo.py
@@ -1,12 +1,19 @@
-from Funcoes import cria_pecas
-
+import Funçoes
 
 i = 0
 while i < 1: #lopping do jogo
 
-    import Funcoes
-    mesa = cria_pecas()
-    print(mesa)
+    num_jogadores = int(input('\nNumero de jogadores (2-4):\n '))
+
+    pecas_jogo = Funçoes.cria_pecas()
+    jogo_inicial = Funçoes.inicia_jogo(num_jogadores, pecas_jogo)
+
+    if num_jogadores == 1:
+        print('\nEssas são suas peças:\n {}'.format(jogo_inicial['jogadores']))
+    elif num_jogadores > 1 and num_jogadores < 4:
+        print('\nEssas são suas peças:\n {}'.format(jogo_inicial['jogadores'][0]))
+    else:
+        print(' ERRO!!!!! ')
 
 
 
@@ -16,7 +23,9 @@ while i < 1: #lopping do jogo
 
 
 
-    continuar = input('Quer continuar jogando?\n') #continuar jogando ou parar lopping
+
+
+    continuar = input('\nQuer continuar jogando?\n ') #continuar jogando ou parar lopping
     if continuar == 'sim': 
         i = 0
     else:
