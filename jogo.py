1b5c875ccd9318f4ea28c1cf694cd43990176d20
pietrobau <pietrob@al.insper.edu.br>
2021-11-05 09:15:27 -0300

    Concerto de alguns erros

--- jogo.py
+++ jogo.py
@@ -2,6 +2,8 @@ import Funçoes
 import random 
 #print ('\033[1m  Your Name  \033[0m')
 
+
+
 x = 0
 while x < 1: #lopping do jogo
 
@@ -16,7 +18,7 @@ while x < 1: #lopping do jogo
         
         for i in peças_monte_mesa['jogadores']:
 
-            print('\033[1m\nEssas são suas peças:\n {}\033[0m'.format(peças_monte_mesa['jogadores'][0]))
+            
          
 
 
@@ -24,32 +26,59 @@ while x < 1: #lopping do jogo
             peça = peças_monte_mesa['jogadores'][i]
             monte = peças_monte_mesa['monte']
             posicoes = Funçoes.posicoes_possiveis(mesa, peça)
-            print('Essas são as posições possiveis:  ', posicoes)
 
-            #print(posicoes)
-            print('Essa é a mesa:   ',mesa)
-            #print('\033[1m\nVez do jogador {}:\033[0m'.format(i))
+            aleatorio = random.choice(monte)
 
-            if peça == []:
-                #print('\033[1m  jogador{} venceu  \033[0m'.format(i))
-                j = 50
-            elif posicoes == [] and monte == []:
-                #print('monte vazio')
-                j = 50
-                vencedor = Funçoes.soma_pecas(peça)
-            elif posicoes == []:
-                #print('se lascou, vai ter que comprar')
-                peças_monte_mesa['jogadores'][i].append(random.choice(monte))
-            elif peça == peças_monte_mesa['jogadores'][0]:
-                #pensar em como destacar as pecas que podem ser jogadas
-                jogada = int(input('qual peça vai jogar?\n '))
-                peças_monte_mesa['mesa'] = Funçoes.adiciona_na_mesa(peça[jogada], mesa)
-                peças_monte_mesa['jogadores'][0].pop(jogada)
+            
+
+            
+
+        
+            
+            
+
+            if peça == peças_monte_mesa['jogadores'][0]:
+
+
+                
+                print('Essa é a mesa:   ',mesa)
+                print('\033[1m\nEssas são suas peças {}:\033[0m'.format(peças_monte_mesa['jogadores'][i]))
+                print(posicoes)
+                if peça == []:
+                    #print('\033[1m  jogador{} venceu  \033[0m'.format(i))
+                    j = 50
+                elif posicoes == [] and monte == []:
+                    #print('monte vazio')
+                    j = 50
+                    vencedor = Funçoes.soma_pecas(peça)
+                elif posicoes == []:
+                    #print('se lascou, vai ter que comprar')
+                    peças_monte_mesa['jogadores'][i].append(aleatorio)
+                    peças_monte_mesa['monte'].remove(aleatorio)
+                elif peça == peças_monte_mesa['jogadores'][0]:
+                    #pensar em como destacar as pecas que podem ser jogadas
+                    jogada = int(input('qual peça vai jogar?\n '))
+                    peças_monte_mesa['mesa'] = Funçoes.adiciona_na_mesa(peça[jogada], mesa)
+                    peças_monte_mesa['jogadores'][0].pop(jogada)
             else:
-                jogada = random.choice(posicoes)
-                peças_monte_mesa['mesa'] = Funçoes.adiciona_na_mesa(peça[jogada], mesa)
-                peças_monte_mesa['jogadores'][i].pop(jogada)
-                print('ele jogou {}'.format(posicoes))
+
+                if peça == []:
+                    #print('\033[1m  jogador{} venceu  \033[0m'.format(i))
+                    j = 50
+                elif posicoes == [] and monte == []:
+                    #print('monte vazio')
+                    j = 50
+                    vencedor = Funçoes.soma_pecas(peça)
+                elif posicoes == []:
+                    #print('se lascou, vai ter que comprar')
+                    peças_monte_mesa['jogadores'][i].append(aleatorio)
+                    peças_monte_mesa['monte'].remove(aleatorio)
+               
+                else:
+                    jogada = random.choice(posicoes)
+                    peças_monte_mesa['mesa'] = Funçoes.adiciona_na_mesa(peça[jogada], mesa)
+                    peças_monte_mesa['jogadores'][i].pop(jogada)
+                    print('O jogador {} jogou'.format(i))
 
 
 
